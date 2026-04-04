88cfd6700bfed357509f01e14b02afde3279a1d8
sumaiya-bhuiyan <sumaiyabhuiyan0105@gmail.com>
2026-04-04 13:18:03 +0600

    Add program to check if a number is even or odd

--- Set-1/p3.java
+++ Set-1/p3.java
@@ -1,1 +1,16 @@
+import java.util.Scanner;
+
+public class Three {
+    public static void main(){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number: ");
+
+        int n=sc.nextInt();
+        boolean isEven=true;
+        if(n%2!=0){
+            isEven=false;
+        }
+        System.out.println(isEven?"Even":"Odd");
+    }
+}
 
