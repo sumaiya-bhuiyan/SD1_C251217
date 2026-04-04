a0da970d6de9458e6bce13c9ea5caa158708de9d
sumaiya-bhuiyan <sumaiyabhuiyan0105@gmail.com>
2026-04-04 13:04:06 +0600

    Update p2.java

--- Set-1/p2.java
+++ Set-1/p2.java
@@ -1,1 +1,11 @@
+import java.util.Scanner;
+
+public class Two {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Your name: ");
+        String name=sc.nextLine();
+        System.out.println("Hello, "+name);
+    }
+}
 
