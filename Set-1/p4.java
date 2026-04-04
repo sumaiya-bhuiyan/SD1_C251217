7e565482af684845c661cd840dc8d748b43f5445
sumaiya-bhuiyan <sumaiyabhuiyan0105@gmail.com>
2026-04-04 13:33:01 +0600

    Add program to find the largest of three numbers

--- Set-1/p4.java
+++ Set-1/p4.java
@@ -1,1 +1,13 @@
+import java.util.Scanner;
 
+public class four {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter three numbers : ");
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.println("The largest number between " + a + "," +b + " and " + c + " is: " + Math.max(a, Math.max(b, c)));
+    }
+}
