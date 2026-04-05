723198db27a432af5388fd2538a8851fd845d582
sumaiya-bhuiyan <sumaiyabhuiyan0105@gmail.com>
2026-04-06 01:23:04 +0600

    Create p9.java

---
+++ Set-1/p9.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Nine{
+    public static void main(String[] args){
+        Scanner helper = new Scanner(System.in);
+        System.out.print("enter the number: ");
+        int n = helper.nextInt();
+
+        int sum = (n * (n + 1)) / 2;
+
+        System.out.println("Sum of 1 to " + n + ": " + sum);
+    }
+}
